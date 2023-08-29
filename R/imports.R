a20135e0abbf18de828e01c17885977822193bf1
Stef van Buuren <stef.vanbuuren@tno.nl>
2023-08-29 16:41:40 +0200

    Add function map_varnames() to transform the JAMES child data into the format used by the CBS model

--- R/imports.R
+++ R/imports.R
@@ -1,7 +1,8 @@
-#' @importFrom dplyr           case_match filter first last mutate pull
+#' @importFrom dplyr           all_of any_of bind_cols case_match filter
+#'                             first last mutate pull rename select
 #' @importFrom centile         y2z
 #' @importFrom nlreferences    set_refcodes
 #' @importFrom rlang           .data
-#' @importFrom tibble          add_column tibble tibble_row
+#' @importFrom tibble          add_column as_tibble tibble tibble_row
 #' @importFrom utils           hasName
 NULL
---
+++ R/map_varnames.R
@@ -0,0 +1,39 @@
+#' Maps variable names from internal to external naming
+#'
+#' Use the internal table `mapping` to transform the internal data into the
+#' form used by the imported model.
+#' @inheritParams collect_predictors
+#'
+#' @param data Target data, e.g. as produced by `collect_predictors()`
+#' @param outcome  Character. Currently only `"overweight-4y"`
+#' @returns A tibble with 1 row containing person data
+#' @examples
+#' library(bdsreader)
+#' fn <- system.file("examples/test.json", package = "bdsreader")
+#' m <- bdsreader::read_bds(fn)
+#' x <- collect_predictors(m)
+#' ex <- map_varnames(x)
+#' @export
+map_varnames <- function(data = NULL,
+                         outcome = "overweight-4y",
+                         version = "20230525") {
+  map <- bdsmodels::mapping |>
+    filter(.data$outcome == !! outcome,
+           .data$version == !! version) |>
+    dplyr::select(all_of(c("ex_name", "in_name")))
+
+  # Rename into external names
+  lookup <- map$in_name
+  names(lookup) <- map$ex_name
+  z <- rename(data, any_of(lookup))
+
+  # Add missing values (double) for any unavailable variables
+  unavailable <- map$ex_name[!map$ex_name %in% names(z)]
+  if (length(unavailable)) {
+    m <- matrix(NA_real_, nrow = 1L, ncol = length(unavailable))
+    colnames(m) <- unavailable
+    z <- add_column(z, as_tibble(m))
+  }
+
+  return(z)
+}
