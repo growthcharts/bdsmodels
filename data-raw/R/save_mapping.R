328c9330064902dae3b7965d0b9eaa8bc1acffb6
Stef van Buuren <stef.vanbuuren@tno.nl>
2023-09-02 11:18:12 +0200

    Extend variable names mapping with version 20230905

--- data-raw/R/save_mapping.R
+++ data-raw/R/save_mapping.R
@@ -1,6 +1,6 @@
 ## Official Country Table used by Dutch Government
 fn <- file.path("data-raw/data/mapping.csv")
 mapping <- read.table(fn, fileEncoding = "macintosh", sep = ";",
-                      header = TRUE, row.names = 1L, dec = ",")
+                      header = TRUE, dec = ",")
 mapping$importance <- as.numeric(mapping$importance)
 usethis::use_data(mapping, overwrite = TRUE)
